5f3334380867941cad71d10e3bfb1b34f8c093a5
souken-b <yuto430scuderia@gmail.com>
2020-07-05 21:29:27 +0900

    11 メッセージの一覧を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:posts).permit(:message, :user_id)
+  end
 end
