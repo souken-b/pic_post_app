514fb2836f0db58edcd3b084e4c51ac342be8c87
souken-b <yuto430scuderia@gmail.com>
2020-07-07 13:43:45 +0900

    15 投稿の詳細ページ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,10 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
